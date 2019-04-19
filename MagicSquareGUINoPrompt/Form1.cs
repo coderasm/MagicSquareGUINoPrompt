05fc881ad699469f3a7a5227350ddba4f5d68a6c
pkrueger <philip.krueger@caci.com>
2019-04-19 15:56:04 -0700

    first push

--- MagicSquareGUINoPrompt/Form1.cs
+++ MagicSquareGUINoPrompt/Form1.cs
@@ -10,11 +10,94 @@ using System.Windows.Forms;
 
 namespace MagicSquareGUINoPrompt
 {
-  public partial class Form1 : Form
+  public partial class magicSquares : Form
   {
-    public Form1()
+    private List<int[,]> grids = new List<int[,]>();
+    private int clientWidth = 900;
+    private int clientHeight = 500;
+    public magicSquares()
     {
       InitializeComponent();
+      ClientSize = new Size(clientWidth, clientHeight);
+      MinimumSize = ClientSize;
+      grids.Add(MagicSquare.Create(3).Grid);
+      grids.Add(MagicSquare.Create(5).Grid);
+      grids.Add(MagicSquare.Create(7).Grid);
+      Invalidate();
+    }
+
+    private void onPaint(object sender, PaintEventArgs e)
+    {
+      drawGrids(e);
+    }
+
+    private void drawGrids(PaintEventArgs e)
+    {
+      var spacing = 10;
+      var smallSquareDimension = 40;
+      var bottom = clientHeight - 10;
+      var spacingBetweenGrids = 20;
+      int[] widths = new int[3];
+      //compute widths
+      for (int i = 0; i < grids.Count; i++)
+      {
+        var nSquares = grids.ElementAt(0).GetLength(0);
+        var width = nSquares * smallSquareDimension + (nSquares + 1) * spacing;
+        widths[0] = width;
+      }
+
+      for (int i = 0; i < grids.Count; i++)
+      {
+        var x = 0;
+        var y = 0;
+        var xOffset = 0;
+        //size all elements
+        var largeSquareDimension = widths[i];
+        //compute offset
+        for (int p = 0; p < widths.Length; p++)
+        {
+          if (p < i)
+            xOffset += widths[p] + spacingBetweenGrids;
+        }
+        x += xOffset;
+        y = bottom - largeSquareDimension;
+        var pen = new Pen(Color.Black);
+        var brush = new SolidBrush(Color.Black);
+
+        //make outer square
+        var largeRect = new Rectangle(x, y, largeSquareDimension, largeSquareDimension);
+        e.Graphics.DrawRectangle(pen, largeRect);
+        e.Graphics.FillRectangle(brush, largeRect);
+
+        pen.Color = Color.LimeGreen;
+        brush.Color = Color.LimeGreen;
+        //move to first square/cell
+        var xInitial = x + spacing;
+        x = xInitial;
+        for (int t = 0; t < grids.ElementAt(t).GetLength(0); t++)
+        {
+          for (int p = 0; p < grids.ElementAt(t).GetLength(1); p++)
+          {
+            //draw small square/cell
+            var smallRect = new Rectangle(x, y, smallSquareDimension, smallSquareDimension);
+            e.Graphics.DrawRectangle(pen, smallRect);
+            e.Graphics.FillRectangle(brush, smallRect);
+            //draw number
+            brush.Color = Color.Black;
+            var font = new Font("Arial", smallSquareDimension * .50F);
+            var textX = x + smallSquareDimension / (grids.ElementAt(t)[t, p] > 9 ? 10 : 4);
+            var textY = y + smallSquareDimension / 5;
+            var format = new StringFormat();
+            e.Graphics.DrawString(grids.ElementAt(t)[t, p].ToString(), font, brush, textX, textY, format);
+            brush.Color = Color.LimeGreen;
+            //move to next cell
+            x = x + smallSquareDimension + spacing;
+          }
+          //move to next row
+          x += xInitial;
+          y = y + smallSquareDimension + spacing;
+        }
+      };
     }
   }
 }
---
+++ MagicSquareGUINoPrompt/MagicSquare.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace MagicSquareGUINoPrompt
+{
+  class MagicSquare
+  {
+    private int nSquares = 1;
+    private int[,] grid;
+    public int[,] Grid {
+      get
+      {
+        return grid;
+      }
+    }
+    private MagicSquare(int nSquares)
+    {
+      this.nSquares = nSquares;
+      generateGrid();
+    }
+
+    public static MagicSquare Create(int nSquares)
+    {
+      return new MagicSquare(nSquares);
+    }
+
+    private void generateGrid()
+    {
+      grid = new int[nSquares, nSquares];
+      if (nSquares % 2 != 0)
+        populateWithOddMethod();
+    }
+
+    private void populateWithOddMethod()
+    {
+      //place one
+      var currentColumn = nSquares / 2;
+      var currentRow = 0;
+      grid[currentRow, currentColumn] = 1;
+      var iterations = nSquares * nSquares - 1;
+      for (int i = 0; i < iterations; i++)
+      {
+        var newRow = currentRow - 1;
+        var newColumn = currentColumn + 1;
+        //outside upper right corner of square
+        if (newRow < 0 && newColumn > nSquares - 1)
+        {
+          grid[++currentRow, currentColumn] = i + 2;
+        }
+        //outside above square
+        else if (newRow < 0 && newColumn <= nSquares - 1)
+        {
+          grid[nSquares - 1, newColumn] = i + 2;
+          currentRow = nSquares - 1;
+          currentColumn = newColumn;
+        }
+        //outside to the right of square
+        else if (newRow <= nSquares - 1 && newColumn > nSquares - 1)
+        {
+          grid[newRow, 0] = i + 2;
+          currentRow = newRow;
+          currentColumn = 0;
+        }
+        else
+        {
+          //check if already occupied
+          if (grid[newRow, newColumn] > 0)
+          {
+            grid[++currentRow, currentColumn] = i + 2;
+          }
+          else
+          {
+            grid[newRow, newColumn] = i + 2;
+            currentRow = newRow;
+            currentColumn = newColumn;
+          }
+        }
+      }
+    }
+
+    public void print()
+    {
+      int[] columnWidths = new int[nSquares];
+      //find column widths
+      for (int i = 0; i < grid.GetLength(0); i++)
+      {
+        for (int p = 0; p < grid.GetLength(1); p++)
+        {
+          var length = grid[i, p].ToString().Length;
+          if (length > columnWidths[p])
+            columnWidths[p] = length;
+        }
+      }
+      //print square
+      for (int i = 0; i < grid.GetLength(0); i++)
+      {
+        var row = "";
+        for (int p = 0; p < grid.GetLength(1); p++)
+        {
+          //get item width
+          var itemWidth = grid[i, p].ToString().Length;
+          var spacesNeeded = 1 + columnWidths[p] - itemWidth;
+          //concat needed spaces
+          for (int t = 0; t < spacesNeeded; t++)
+          {
+            row += " ";
+          }
+          row += $"{grid[i, p]}";
+        }
+        Console.WriteLine(row);
+      }
+    }
+  }
+}
